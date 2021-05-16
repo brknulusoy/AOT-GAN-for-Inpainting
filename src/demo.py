ab1b09df893a80350318024f6ad555d0b7be6147
Deniz Ugur <deniz343@gmail.com>
2021-05-16 15:47:37 +0300

    update demo code

--- src/demo.py
+++ src/demo.py
@@ -1,10 +1,12 @@
 import importlib
+import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
 from torchvision.transforms import ToTensor
 
 from utils.option import args
+from metric import metric as module_metric
 from data.dataset import TerrainDataset
 
 
@@ -13,6 +15,8 @@ def demo(args):
         args.dir_train,
         dataset_type="train",
         randomize=True,
+        random_state=343,
+        idx_offset=args.idx_offset,
         block_variance=1,
     )
 
@@ -22,7 +26,7 @@ def demo(args):
     model.load_state_dict(torch.load(args.pre_train, map_location="cpu"))
     model.eval()
 
-    for target, mask, fn in dataset:
+    for target, mask, fn, _ in dataset:
         print(f"[**] inpainting {fn} ... ")
         target = target.unsqueeze(0)
         mask = mask.unsqueeze(0)
@@ -32,19 +36,41 @@ def demo(args):
             pred_tensor = model(masked_tensor, mask)
             comp_tensor = pred_tensor * mask + target * (1 - mask)
 
-            fig, axs = plt.subplots(2, 2, figsize=(12, 12), sharex=True, sharey=False)
+            # metrics prepare for image assesments
+            metrics = {
+                met: getattr(module_metric, met) for met in ["mae", "psnr", "ssim"]
+            }
+            evaluation_scores = {key: 0 for key, val in metrics.items()}
+            for key, val in metrics.items():
+                evaluation_scores[key] = val(
+                    target.numpy().reshape((1, 256, 256)),
+                    comp_tensor.numpy().reshape((1, 256, 256)),
+                    num_worker=1,
+                )
+
+            fig, axs = plt.subplots(2, 2, figsize=(12, 12), sharex=True, sharey=True)
 
             target = target.reshape((256, 256))
             mask = mask.reshape((256, 256))
-            masked_tensor = masked_tensor.reshape((256, 256))
+            masked = np.copy(target.numpy())
+            masked[mask == 1] = np.nan
             comp_tensor = comp_tensor.reshape((256, 256))
 
             axs[0, 0].contourf(target.numpy(), levels=100, cmap="terrain")
             axs[0, 1].contourf(mask.numpy(), levels=100, cmap="gray")
-            axs[1, 0].contourf(masked_tensor.numpy(), levels=100, cmap="terrain")
+            axs[1, 0].contourf(masked, levels=100, cmap="terrain")
             axs[1, 1].contourf(comp_tensor.numpy(), levels=100, cmap="terrain")
 
-            fig.tight_layout()
+            fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+            fig.suptitle(
+                " ".join(
+                    [
+                        "{}: {:6f}".format(key, val)
+                        for key, val in evaluation_scores.items()
+                    ],
+                ),
+                fontsize=22,
+            )
             plt.show()
 
 
