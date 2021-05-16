2e46dbe7a8de7637e171e5db7667cd9da3b7d090
Deniz Ugur <deniz343@gmail.com>
2021-05-16 13:09:07 +0300

    simpler demo code

--- src/demo.py
+++ src/demo.py
@@ -1,112 +1,52 @@
-import cv2
-import os
 import importlib
-import numpy as np
-from glob import glob 
+import matplotlib.pyplot as plt
 
 import torch
 from torchvision.transforms import ToTensor
 
 from utils.option import args
-from utils.painter import Sketcher
-
-
-
-def postprocess(image):
-    image = torch.clamp(image, -1., 1.)
-    image = (image + 1) / 2.0 * 255.0
-    image = image.permute(1, 2, 0)
-    image = image.cpu().numpy().astype(np.uint8)
-    return image
-
+from data.dataset import TerrainDataset
 
 
 def demo(args):
-    # load images 
-    img_list = []
-    for ext in ['*.jpg', '*.png']: 
-        img_list.extend(glob(os.path.join(args.dir_image, ext)))
-    img_list.sort()
+    dataset = TerrainDataset(
+        args.dir_train,
+        dataset_type="train",
+        randomize=True,
+        block_variance=1,
+    )
 
     # Model and version
-    net = importlib.import_module('model.'+args.model)
+    net = importlib.import_module("model." + args.model)
     model = net.InpaintGenerator(args)
-    model.load_state_dict(torch.load(args.pre_train, map_location='cpu'))
+    model.load_state_dict(torch.load(args.pre_train, map_location="cpu"))
     model.eval()
 
-    for fn in img_list:
-        filename = os.path.basename(fn).split('.')[0]
-        orig_img = cv2.resize(cv2.imread(fn, cv2.IMREAD_COLOR), (512, 512))
-        img_tensor = (ToTensor()(orig_img) * 2.0 - 1.0).unsqueeze(0)
-        h, w, c = orig_img.shape
-        mask = np.zeros([h, w, 1], np.uint8)
-        image_copy = orig_img.copy()
-        sketch = Sketcher(
-            'input', [image_copy, mask], lambda: ((255, 255, 255), (255, 255, 255)), args.thick, args.painter)
+    for target, mask, fn in dataset:
+        print(f"[**] inpainting {fn} ... ")
+        target = target.unsqueeze(0)
+        mask = mask.unsqueeze(0)
 
-        while True:
-            ch = cv2.waitKey()
-            if ch == 27:
-                print("quit!")
-                break
+        with torch.no_grad():
+            masked_tensor = (target * (1 - mask).float()) + mask
+            pred_tensor = model(masked_tensor, mask)
+            comp_tensor = pred_tensor * mask + target * (1 - mask)
 
-            # inpaint by deep model
-            elif ch == ord(' '):
-                print('[**] inpainting ... ')
-                with torch.no_grad():
-                    mask_tensor = (ToTensor()(mask)).unsqueeze(0)
-                    masked_tensor = (img_tensor * (1 - mask_tensor).float()) + mask_tensor
-                    pred_tensor = model(masked_tensor, mask_tensor)
-                    comp_tensor = (pred_tensor * mask_tensor + img_tensor * (1 - mask_tensor))
+            fig, axs = plt.subplots(2, 2, figsize=(12, 12), sharex=True, sharey=False)
 
-                    pred_np = postprocess(pred_tensor[0])
-                    masked_np = postprocess(masked_tensor[0])
-                    comp_np = postprocess(comp_tensor[0])
+            target = target.reshape((256, 256))
+            mask = mask.reshape((256, 256))
+            masked_tensor = masked_tensor.reshape((256, 256))
+            comp_tensor = comp_tensor.reshape((256, 256))
 
-                    cv2.imshow('pred_images', comp_np)
-                    print('inpainting finish!')
-            
-            # reset mask
-            elif ch == ord('r'):
-                img_tensor = (ToTensor()(orig_img) * 2.0 - 1.0).unsqueeze(0)
-                image_copy[:] = orig_img.copy()
-                mask[:] = 0
-                sketch.show()
-                print("[**] reset!")
+            axs[0, 0].contourf(target.numpy(), levels=100, cmap="terrain")
+            axs[0, 1].contourf(mask.numpy(), levels=100, cmap="gray")
+            axs[1, 0].contourf(masked_tensor.numpy(), levels=100, cmap="terrain")
+            axs[1, 1].contourf(comp_tensor.numpy(), levels=100, cmap="terrain")
 
-            # next case
-            elif ch == ord('n'):
-                print('[**] move to next image')
-                cv2.destroyAllWindows()
-                break
-
-            elif ch == ord('k'): 
-                print('[**] apply existing processing to images, and keep editing!')
-                img_tensor = comp_tensor
-                image_copy[:] = comp_np.copy()
-                mask[:] = 0
-                sketch.show()
-                print("reset!")
-            
-            elif ch == ord('+'): 
-                sketch.large_thick()
-
-            elif ch == ord('-'): 
-                sketch.small_thick()
-            
-            # save results
-            if ch == ord('s'):
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_masked.png'), masked_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_pred.png'), pred_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_comp.png'), comp_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_mask.png'), mask)
-
-                print('[**] save successfully!')
-        cv2.destroyAllWindows()
-
-        if ch == 27:
-            break
+            fig.tight_layout()
+            plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     demo(args)
